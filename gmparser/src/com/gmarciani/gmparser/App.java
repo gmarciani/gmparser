66d1effa19fc0c9c21e4084990aa4e47ecb79004
giacomomarciani <giacomo.marciani@gmail.com>
2014-05-16 21:16:24 +0200

    App Main

---
+++ gmparser/src/com/gmarciani/gmparser/App.java
@@ -0,0 +1,20 @@
+package com.gmarciani.gmparser;
+
+import org.apache.commons.cli.ParseException;
+
+import com.gmarciani.gmparser.controllers.AppController;
+
+public class App {
+
+	public static void main(String[] args) {	
+		AppController app = AppController.getInstance();
+		app.printWelcome();
+		try {
+			app.play(args);
+		} catch (ParseException exc) {
+			app.printException(exc);
+			app.quit();
+		}	
+	}	
+
+}
